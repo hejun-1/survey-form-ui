f817e56f3664df0fc667f274b3f91fa18d483a30
Li Cheng <chengli@thoughtworks.com>
2017-09-11 06:44:32 +0800

    Map port number

--- buildtask/docker-run.sh
+++ buildtask/docker-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -p 8080:80 --rm --name=survey-form-ui registry.cn-beijing.aliyuncs.com/stardust/survey-form-ui
+docker run -it -p 80:80 --rm --name=survey-form-ui registry.cn-beijing.aliyuncs.com/stardust/survey-form-ui
