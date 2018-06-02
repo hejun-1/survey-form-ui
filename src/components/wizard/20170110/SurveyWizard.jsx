fa1449be75ed9184b8c830260c6a31f3d2b05cd5
Cheng <cheng.li@rea-group.com>
2018-06-02 21:53:57 +0800

    Close form 20170110

--- src/components/wizard/20170110/SurveyWizard.jsx
+++ src/components/wizard/20170110/SurveyWizard.jsx
@@ -4,9 +4,10 @@ import Wizard from '../Wizard';
 import StepSurvey from './StepSurvey';
 import StepUploadPicture from './StepUploadPicture';
 import StepComplete from './StepComplete';
+import StepClosed from '../StepClosed';
 
 const steps = [
-  StepSurvey, StepUploadPicture, StepComplete
+  StepClosed
 ];
 
 const SurveyWizard = () => <Wizard steps={steps}/>;
