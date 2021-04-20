c32f150091fb4bb62b7d5f357c576821ec5e2927
hejun-1 <80243536+hejun-1@users.noreply.github.com>
2021-04-20 19:49:26 +0800

    Update docker-push.sh

--- buildtask/docker-push.sh
+++ buildtask/docker-push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+docker login --username=$DOCKER_REGISTRY_USER --password=$DOCKER_REGISTRY_PWD registry.cn-hangzhou.aliyuncs.com
 docker tag registry.cn-hangzhou.aliyuncs.com/stardust/survey-form-ui-new registry.cn-hangzhou.aliyuncs.com/stardust/survey-form-ui-new:latest
 docker push registry.cn-hangzhou.aliyuncs.com/stardust/survey-form-ui-new:latest
