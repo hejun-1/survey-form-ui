b324f32c6f7932bcf9ad8c5086554c7b38b63f54
hejun-1 <hejun.1688@163.com>
2021-04-20 21:16:19 +0800

    增加老挝lang

---
+++ src/components/result-report/langLW.jsx
@@ -0,0 +1,42 @@
+const LANG = {};
+
+//ReportViewer.jsx
+LANG["HIV_DNA_QUALITATIVE_TEST"] = "HIV-DNAການກວດວິເຄາະ";
+LANG["NEGATIVE"] = "ຜົນລົບ";
+LANG["URINE_HIV_ANTIBODY_TEST"] = "ການກວດຕ້ານເຊື້ອ HIV ໃນຍ່ຽວ";
+LANG["HIV_ANTIBODY_TEST"] = "ການກວດຕ້ານເຊື້ອ HIV";
+LANG["SAMPLE_CODE"] = "ລະຫັດຕົວຢ່າງ";
+LANG["SAMPLE_TYPE"] = "ປະເພດຕົວຢ່າງ";
+LANG["TEST_METHOD"] = "ວິທີການກວດ";
+LANG["TEST_DELIVERY_METHOD"] = "ວິທີການຈັດສົ່ງເພື່ອກວດ"; 
+LANG["TEST_INSTITUTION"] = "ອົງການກວດ";
+LANG["TEST_ITEM"] = "ລາຍການກວດ";
+LANG["RESULT"] = "ຜົນກວດ";
+LANG["REFERENCE_VALUE"] = "ຂໍ້ມູນອ້າງອີງ";
+LANG["TEST_PEOPLE"] = "ຜູ້ກວດ";
+LANG["SAMPLE_RECEIVE_DATE"] = "ວັນທີທີ່ໄດ້ຮັບຕົວຢ່າງ";
+LANG["REPORT_DATE"] = "ວັນທີລາຍງານ";
+LANG["YEAR"] = "ປີ";
+LANG["MONTH"] = "ເດືອນ";
+LANG["DAY"] = "ວັນ";
+LANG["NOTE"] = "ຫມາຍເຫດ";
+
+//ReportSearch.jsx
+LANG["SAMPLE_CODE_ERROR_OR_UNPUBLISHED_REPORT"] = "ຕົວເລກຕົວຢ່າງແມ່ນບໍ່ຖືກຕ້ອງຫຼືລາຍງານດັ່ງກ່າວຍັງບໍ່ໄດ້ຖືກເຜີຍແຜ່ເທື່ອ";
+LANG["SAMPLE_DELIVERED_WAIT_5_DAYS"] = "ພາຍໃຕ້ສະພາບປົກກະຕິ, ຫຼັງຈາກໄດ້ຮັບຕົວຢ່າງ, ອົງການກວດຈະໃຫ້ຜົນກວດໃນເວລາປະມານ 5 ວັນເຮັດວຽກ.";
+
+LANG["WARNING_FOR_RESULT, ກະລຸນາກວດສອບໃຫ້ແນ່ໃຈ່ວ່າທ່ານສາມາດຍອມຮັບຜົນການກວດໄດ້，ຖ້າທ່ານມີຄຳຖາມຕ້ອງຂໍຄວາມຊ່ວຍເຫຼືອໃຫ້ທັນເວລາ"] ="ທ່ານສາມາດຕິດຕໍ່ພວກເຮົາໄດ້ຕາມຊ່ອງທາງຕໍ່ໄປນີ້"; 
+LANG["CONTACT_INFO"] = "ຂໍ້​ມູນ​ຕິດ​ຕໍ່";
+
+LANG["CONSULT_INSTRUCTION_1"] = "";
+
+LANG["CONSULT_INSTRUCTION_2"] = "";
+
+LANG["CONSULT_INSTRUCTION_3"] = "";
+
+LANG["ENTER_SAMPLE_CODE"] = "ໃສ່ໝາຍເລກຕົວຢ່າງ";
+
+LANG["SEARCH"] = "ຄົ້ນຫາ";
+
+
+export default LANG;
